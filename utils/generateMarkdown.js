aab265a66af72683f818fc09552882222b3907e2
BellaAdrien <Iadrien2020@fau.edu>
2023-10-26 17:01:28 -0400

    save9

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,10 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
+if(license==="MIT"){
+  return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+} else if
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -26,8 +30,9 @@ function generateMarkdown(data) {
   
   - [Installation](#installation)
   - [Usage](#usage)
-  - [Credits](#credits)
+  - [Contribution](#contribution)
   - [License](#license)
+  - [Tests](#tests)
   
   ## Installation
   ${data.installation}
